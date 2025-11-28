6f72d7a3c3dc26065774398f67f6ce34441e5d02
Madrigal <improbable2510@gmail.com>
2025-11-27 22:43:32 -0600

    Revert "Merge branch 'Juan'"
    This reverts commit e8e5604e5b4ac7025aeb4422f981271ce82dd25e, reversing
    changes made to 3e616e95c96292469532f573c0135fc4192f8a6d.

--- ProyectoAhorra/ScreensAhorra/PantallaInicio.js
+++ ProyectoAhorra/ScreensAhorra/PantallaInicio.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  Dimensions
-} from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native';
 
 const { height, width } = Dimensions.get('window');
 
-export default function InicioScreen({ navigation }) {
+export default function PantallaInicio() {
   return (
     <View style={styles.page}>
       <View style={styles.phoneContainer}>
 
-        {/* Contenedor azul */}
-
         {/* CONTENEDOR AZUL */}
         <View style={styles.blueContainerWrapper}>
           <View style={styles.blueBackground} />
-
-          <View style={styles.blueContent}>
-            <Text style={styles.title}>¡AHORRA MÁS!</Text>
-
-            <Image
-              source={require('../assets/cerdito_sinfondo.png')}
-              style={styles.cerdito}
-            />
-          </View>
-        </View>
           
           <View style={styles.blueContent}>
             <Text style={styles.title}>¡AHORRA MÁS!</Text>
@@ -40,24 +21,13 @@ export default function InicioScreen({ navigation }) {
           </View>
         </View>
 
-        {/* Botón Acceso */}
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('Acceso')}
-        >
-          <Text style={styles.buttonText}>Acceso</Text>
-        </TouchableOpacity>
         {/* PARTE INFERIOR */}
         <View style={styles.bottomContainer}>
           <TouchableOpacity style={styles.button}>
             <Text style={styles.buttonText}>Acceso</Text>
           </TouchableOpacity>
 
-        {/* Link Registro */}
-        <TouchableOpacity onPress={() => navigation.navigate('Registro')}>
           <Text style={styles.link}>¿Aún no tienes cuenta?</Text>
-        </TouchableOpacity>
-
         </View>
 
       </View>
@@ -68,7 +38,7 @@ export default function InicioScreen({ navigation }) {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    backgroundColor: '#1e1e1e',
+    backgroundColor: '#cfe3ff',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -77,7 +47,7 @@ const styles = StyleSheet.create({
     width: 380,
     height: 750,
     backgroundColor: '#fff',
-    borderRadius: 0,
+    borderRadius: 25,
     alignItems: 'center',
     overflow: 'hidden',
 
@@ -96,25 +66,6 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
 
-  blueContainerWrapper: {
-    width: '100%',
-    height: '65%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    overflow: 'hidden',
-    position: 'relative',
-  },
-
-  blueBackground: {
-    backgroundColor: '#00A6FF',
-    width: '150%',
-    height: '140%',
-    borderBottomRightRadius: 100,
-    transform: [{ rotate: '30deg' }],
-    position: 'absolute',
-    top: -300,
-    left: '-25%',
-  },
   blueBackground: {
     backgroundColor: '#00A6FF',
     width: '150%',
@@ -133,22 +84,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '90%',
   },
-  blueContent: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    width: '100%',
-    height: '90%',
-  },
 
-  title: {
-    color: '#fff',
-    fontSize: 40,
-    fontWeight: '900',
-    textAlign: 'center',
-    width: '100%',
-    marginTop: -20,
-  },
   title: {
     color: '#fff',
     fontSize: 40,
@@ -158,12 +94,6 @@ const styles = StyleSheet.create({
     width: '100%',
   },
 
-  cerdito: {
-    width: 220,
-    height: 220,
-    position: 'absolute',
-    bottom: -40,
-    right: '5%',
   cerdito: {
     width: 220,
     height: 220,
@@ -190,7 +120,6 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 4,
     elevation: 4,
-    marginTop: 20,
   },
 
   buttonText: {
@@ -202,7 +131,5 @@ const styles = StyleSheet.create({
   link: {
     color: '#7F3DFF',
     fontSize: 15,
-    marginTop: 12,
   },
 });
-
